d54675c00af76978e59224cca16738e23bcee0dd
RubenVBarros <rubenvenancio67@gmail.com>
2021-06-09 17:25:21 +0200

    main: ajout des autres directions pour le titre de l objet

--- objet2.js
+++ objet2.js
@@ -5,7 +5,7 @@ let data =[
         "texte":"Molécule de cristal",
         "description":"C'est une molécule de cristal, \nune molécule c'est très petit et \nc'est l'assemblage de plusieurs \natomes.",
         "path":"assets/cristal.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":1e-6,
@@ -13,7 +13,7 @@ let data =[
         "texte":"Globule rouge",
         "description":"Les globules rouges permettent \nde transporter l'oxygène vers les \ntissus et elles permettent de \ncapter le gaz carbonique au \nniveau des tissus pour les \néliminer par les voies respiratoires.",
         "path":"assets/globule-rouge.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":3e-3,
@@ -21,7 +21,7 @@ let data =[
         "texte":"Larve de poisson",
         "description":"La larve de poisson est le premier \nstade de développement pour un \npoisson, après l'éclosion des \noeufs, c'est comme cela que le \npoisson débute sa vie.",
         "path":"assets/larve.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":1e-3,
@@ -29,7 +29,7 @@ let data =[
         "texte":"Poisson zèbre",
         "description":"Le poisson zèbre est une espèce \nde poisson très utilisée dans \nle monde scientifique car il aurait \nla particularité de régénérer \ndes parties de son corps en cas \nde blessure, si on pouvait \nappliquer cela aux humains ce \nserait fou !",
         "path":"assets/poisson.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":2e-1,
@@ -37,7 +37,7 @@ let data =[
         "texte":"Main",
         "description":"Une main, nous en avons deux \nmais une seule fait près de 20 \ncentimètres, rendez-vous compte \nla taille d'un double décimètre \nmais on peut faire tellement de \nchoses avec, que feriez vous si \nvous n'aviez pas de main ?",
         "path":"assets/main.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":12,
@@ -45,7 +45,7 @@ let data =[
         "texte":"Bus",
         "description":"Ca c'est le ScientiBus, c'est la ou \nont lieu les meilleures expériences \nscientifiques de France.",
         "path":"assets/bus.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":128,
@@ -53,7 +53,7 @@ let data =[
         "texte":"Cathédrale de Notre Dame",
         "description":"La cathédrale Notre-Dame de \nParis est un monument \nemblématique de Paris bien \nqu'elle ait subit un incendie, son \nhistoire ne s'en trouve pas moins \neffacé. Elle a accueilli le Sacre de \nNapoléon en 1804.",
         "path":"assets/notre-dame.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":8.8e3,
@@ -61,23 +61,23 @@ let data =[
         "texte":"Ville de Limoges",
         "description":"La ville de Limoges avec ses \nagglomérations autour, c'est la \nville ou vous habitez sûrement !",
         "path":"assets/limoges.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":2.592402e6,
         "angle":Math.PI/7,
         "texte":"France",
-        "description":"C'est la France, pays dans lequel \nnous vivons, c'est le pays de la \nbaguette et du bon vin.",
+        "description":"C'est la France, pays dans lequel \nnous vivons, c'nord le pays de la \nbaguette et du bon vin.",
         "path":"assets/france.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":12742000.0,
         "angle":Math.PI/6,
         "texte":"Terre",
-        "description":"La Terre, c'est la planète ou nous \nvivons, le saviez-vous que la Terre \nest composé à 70% d'eau, elle \nréunit aussi tout les critères \nnécessaires pour le \ndéveloppement de la vie.",
+        "description":"La Terre, c'est la planète ou nous \nvivons, le saviez-vous que la Terre \eest composé à 70% d'eau, elle \nréunit aussi tout les critères \nnécessaires pour le \ndéveloppement de la vie.",
         "path":"assets/earth.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":116460000.0,
@@ -85,7 +85,7 @@ let data =[
         "texte":"Saturne",
         "description":"Saturne est la seule planète qui \npossède une couronne d'anneaux \net elle possède plus d'une \ncentaine de satellite !",
         "path":"assets/saturn.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":1.4e9,
@@ -93,7 +93,7 @@ let data =[
         "texte":"Soleil",
         "description":"Le Soleil est notre source de \nlumières et de chaleur, le soleil est \ntellement massif qu'il représente \nplus de 99,854% de la masse \ntotale du Système solaire et il \német 20 000 fois plus d'énergie \nque nous en consommons, c'est \ntotalement dingue !",
         "path":"assets/sun.png",
-        "orientation":"ouest"
+        "orientation":"est"
     },
     {
         "tailleReelle":1000000.0,
@@ -101,6 +101,6 @@ let data =[
         "texte":"Mars",
         "description":"La planete rouge !",
         "path":"assets/mars.png",
-        "orientation":"ouest"
+        "orientation":" est"
     }
 ];
