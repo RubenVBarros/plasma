8d178ff1d75ba0bfa3c9d788ee1268acc182835f
RubenVBarros <rubenvenancio67@gmail.com>
2021-06-11 12:13:07 +0200

    main: rectif puissance

--- objet2.js
+++ objet2.js
@@ -3,117 +3,117 @@ let data =[
         "tailleReelle":1e-9,
         "angle":Math.PI/8,
         "texte":"Molécule de cristal",
+        "positionTexteX":100,
+        "positionTexteY":100,
         "description":"C'est une molécule de cristal, \nune molécule c'est très petit et \nc'est l'assemblage de plusieurs \natomes.",
-        "path":"assets/cristal.png",
-        "orientationX":100,
-        "orientationY":100
+        "path":"assets/cristal.png"
     },
     {
         "tailleReelle":1e-6,
         "angle":Math.PI/8,
         "texte":"Globule rouge",
+        "positionTexteX":100,
+        "positionTexteY":100,
         "description":"Les globules rouges permettent \nde transporter l'oxygène vers les \ntissus et elles permettent de \ncapter le gaz carbonique au \nniveau des tissus pour les \néliminer par les voies respiratoires.",
-        "path":"assets/globule-rouge.png",
-        "orientationX":100,
-        "orientationY":100
+        "path":"assets/globule-rouge.png"
     },
     {
         "tailleReelle":3e-3,
         "angle":Math.PI/7,
         "texte":"Larve de poisson",
+        "positionTexteX":-800,
+        "positionTexteY":600,
         "description":"La larve de poisson est le premier \nstade de développement pour un \npoisson, après l'éclosion des \noeufs, c'est comme cela que le \npoisson débute sa vie.",
-        "path":"assets/larve.png",
-        "orientationX":-800,
-        "orientationY":600
+        "path":"assets/larve.png"
     },
     {
         "tailleReelle":1e-3,
         "angle":Math.PI/7,
         "texte":"Poisson zèbre",
+        "positionTexteX":100,
+        "positionTexteY":100,
         "description":"Le poisson zèbre est une espèce \nde poisson très utilisée dans \nle monde scientifique car il aurait \nla particularité de régénérer \ndes parties de son corps en cas \nde blessure, si on pouvait \nappliquer cela aux humains ce \nserait fou !",
-        "path":"assets/poisson.png",
-        "orientationX":100,
-        "orientationY":100
+        "path":"assets/poisson.png"
     },
     {
         "tailleReelle":2e-1,
         "angle":Math.PI/8,
         "texte":"Main",
+        "positionTexteX":100,
+        "positionTexteY":100,
         "description":"Une main, nous en avons deux \nmais une seule fait près de 20 \ncentimètres, rendez-vous compte \nla taille d'un double décimètre \nmais on peut faire tellement de \nchoses avec, que feriez vous si \nvous n'aviez pas de main ?",
-        "path":"assets/main.png",
-        "orientationX":100,
-        "orientationY":100
+        "path":"assets/main.png"
     },
     {
         "tailleReelle":12,
         "angle":Math.PI/4,
         "texte":"Bus",
+        "positionTexteX":2000,
+        "positionTexteY":-200,
         "description":"Ca c'est le ScientiBus, c'est la ou \nont lieu les meilleures expériences \nscientifiques de France.",
-        "path":"assets/bus.png",
-        "orientationX":2000,
-        "orientationY":-200
+        "path":"assets/bus.png"
     },
     {
         "tailleReelle":128,
         "angle":Math.PI/7,
         "texte":"Cathédrale de Notre Dame",
+        "positionTexteX":750,
+        "positionTexteY":100,
         "description":"La cathédrale Notre-Dame de \nParis est un monument \nemblématique de Paris bien \nqu'elle ait subit un incendie, son \nhistoire ne s'en trouve pas moins \neffacé. Elle a accueilli le Sacre de \nNapoléon en 1804.",
-        "path":"assets/notre-dame.png",
-        "orientationX":750,
-        "orientationY":100    
+        "path":"assets/notre-dame.png"
     },
     {
         "tailleReelle":8.8e3,
         "angle":Math.PI/7,
         "texte":"Ville de Limoges",
+        "positionTexteX":100,
+        "positionTexteY":100,  
         "description":"La ville de Limoges avec ses \nagglomérations autour, c'est la \nville ou vous habitez sûrement !",
-        "path":"assets/limoges.png",
-        "orientationX":100,
-        "orientationY":100    
+        "path":"assets/limoges.png"
     },
     {
         "tailleReelle":2.592402e6,
         "angle":Math.PI/7,
         "texte":"France",
+        "positionTexteX":500,
+        "positionTexteX":650,
         "description":"C'est la France, pays dans lequel \nnous vivons, c'nord le pays de la \nbaguette et du bon vin.",
-        "path":"assets/france.png",
-        "orientationX":500,
-        "orientationY":650   
+        "path":"assets/france.png"
     },
     {
         "tailleReelle":12742000.0,
         "angle":Math.PI/6,
         "texte":"Terre",
+        "positionTexteX":100,
+        "positionTexteY":600,  
         "description":"La Terre, c'est la planète ou nous \nvivons, le saviez-vous que la Terre \eest composé à 70% d'eau, elle \nréunit aussi tout les critères \nnécessaires pour le \ndéveloppement de la vie.",
-        "path":"assets/earth.png",
-        "orientationX":100,
-        "orientationY":600   
+        "path":"assets/earth.png"
     },
     {
         "tailleReelle":116460000.0,
         "angle":Math.PI/6,
         "texte":"Saturne",
+        "positionTexteX":100,
+        "positionTexteY":600,
         "description":"Saturne est la seule planète qui \npossède une couronne d'anneaux \net elle possède plus d'une \ncentaine de satellite !",
-        "path":"assets/saturn.png",
-        "orientationX":100,
-        "orientationY":600   
+        "path":"assets/saturn.png"
     },
     {
         "tailleReelle":1.4e9,
         "angle":Math.PI/7,
         "texte":"Soleil",
+        "positionTexteX":100,
+        "positionTexteY":600,  
         "description":"Le Soleil est notre source de \nlumières et de chaleur, le soleil est \ntellement massif qu'il représente \nplus de 99,854% de la masse \ntotale du Système solaire et il \német 20 000 fois plus d'énergie \nque nous en consommons, c'est \ntotalement dingue !",
-        "path":"assets/sun.png",
-        "orientationX":100,
-        "orientationY":600   
+        "path":"assets/sun.png"
     },
     {
         "tailleReelle":1000000.0,
         "angle":Math.PI/7,
         "texte":"Mars",
+        "positionTexteX":500,
+        "positionTexteY":600, 
         "description":"La planete rouge !",
-        "path":"assets/mars.png",
-        "orientationX":500,
-        "orientationY":600   
+        "path":"assets/mars.png"
     }
 ];
