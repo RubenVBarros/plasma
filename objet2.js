250a2fdffa7e36e22d0b6a2d9d5677a2456cb7cb
RubenVBarros <rubenvenancio67@gmail.com>
2021-06-11 13:35:59 +0200

    main: rectif position titre selon images

--- objet2.js
+++ objet2.js
@@ -3,8 +3,8 @@ let data =[
         "tailleReelle":1e-9,
         "angle":Math.PI/8,
         "texte":"Molécule de cristal",
-        "positionTexteX":100,
-        "positionTexteY":100,
+        "positionTexteX":300,
+        "positionTexteY":-400,
         "description":"C'est une molécule de cristal, \nune molécule c'est très petit et \nc'est l'assemblage de plusieurs \natomes.",
         "path":"assets/cristal.png"
     },
@@ -12,8 +12,8 @@ let data =[
         "tailleReelle":1e-6,
         "angle":Math.PI/8,
         "texte":"Globule rouge",
-        "positionTexteX":100,
-        "positionTexteY":100,
+        "positionTexteX":-1000,
+        "positionTexteY":0,
         "description":"Les globules rouges permettent \nde transporter l'oxygène vers les \ntissus et elles permettent de \ncapter le gaz carbonique au \nniveau des tissus pour les \néliminer par les voies respiratoires.",
         "path":"assets/globule-rouge.png"
     },
@@ -21,8 +21,8 @@ let data =[
         "tailleReelle":3e-3,
         "angle":Math.PI/7,
         "texte":"Larve de poisson",
-        "positionTexteX":-800,
-        "positionTexteY":600,
+        "positionTexteX":-1300,
+        "positionTexteY":0,
         "description":"La larve de poisson est le premier \nstade de développement pour un \npoisson, après l'éclosion des \noeufs, c'est comme cela que le \npoisson débute sa vie.",
         "path":"assets/larve.png"
     },
@@ -31,7 +31,7 @@ let data =[
         "angle":Math.PI/7,
         "texte":"Poisson zèbre",
         "positionTexteX":100,
-        "positionTexteY":100,
+        "positionTexteY":-500,
         "description":"Le poisson zèbre est une espèce \nde poisson très utilisée dans \nle monde scientifique car il aurait \nla particularité de régénérer \ndes parties de son corps en cas \nde blessure, si on pouvait \nappliquer cela aux humains ce \nserait fou !",
         "path":"assets/poisson.png"
     },
@@ -39,8 +39,8 @@ let data =[
         "tailleReelle":2e-1,
         "angle":Math.PI/8,
         "texte":"Main",
-        "positionTexteX":100,
-        "positionTexteY":100,
+        "positionTexteX":-500,
+        "positionTexteY":500,
         "description":"Une main, nous en avons deux \nmais une seule fait près de 20 \ncentimètres, rendez-vous compte \nla taille d'un double décimètre \nmais on peut faire tellement de \nchoses avec, que feriez vous si \nvous n'aviez pas de main ?",
         "path":"assets/main.png"
     },
@@ -48,8 +48,8 @@ let data =[
         "tailleReelle":12,
         "angle":Math.PI/4,
         "texte":"Bus",
-        "positionTexteX":2000,
-        "positionTexteY":-200,
+        "positionTexteX":1200,
+        "positionTexteY":-700,
         "description":"Ca c'est le ScientiBus, c'est la ou \nont lieu les meilleures expériences \nscientifiques de France.",
         "path":"assets/bus.png"
     },
@@ -57,8 +57,8 @@ let data =[
         "tailleReelle":128,
         "angle":Math.PI/7,
         "texte":"Cathédrale de Notre Dame",
-        "positionTexteX":750,
-        "positionTexteY":100,
+        "positionTexteX":-400,
+        "positionTexteY":-600,
         "description":"La cathédrale Notre-Dame de \nParis est un monument \nemblématique de Paris bien \nqu'elle ait subit un incendie, son \nhistoire ne s'en trouve pas moins \neffacé. Elle a accueilli le Sacre de \nNapoléon en 1804.",
         "path":"assets/notre-dame.png"
     },
@@ -66,8 +66,8 @@ let data =[
         "tailleReelle":8.8e3,
         "angle":Math.PI/7,
         "texte":"Ville de Limoges",
-        "positionTexteX":100,
-        "positionTexteY":100,  
+        "positionTexteX":0,
+        "positionTexteY":-500,  
         "description":"La ville de Limoges avec ses \nagglomérations autour, c'est la \nville ou vous habitez sûrement !",
         "path":"assets/limoges.png"
     },
@@ -75,8 +75,8 @@ let data =[
         "tailleReelle":2.592402e6,
         "angle":Math.PI/7,
         "texte":"France",
-        "positionTexteX":500,
-        "positionTexteX":650,
+        "positionTexteX":-300,
+        "positionTexteY":300,
         "description":"C'est la France, pays dans lequel \nnous vivons, c'nord le pays de la \nbaguette et du bon vin.",
         "path":"assets/france.png"
     },
@@ -84,8 +84,8 @@ let data =[
         "tailleReelle":12742000.0,
         "angle":Math.PI/6,
         "texte":"Terre",
-        "positionTexteX":100,
-        "positionTexteY":600,  
+        "positionTexteX":-400,
+        "positionTexteY":0,  
         "description":"La Terre, c'est la planète ou nous \nvivons, le saviez-vous que la Terre \eest composé à 70% d'eau, elle \nréunit aussi tout les critères \nnécessaires pour le \ndéveloppement de la vie.",
         "path":"assets/earth.png"
     },
@@ -94,7 +94,7 @@ let data =[
         "angle":Math.PI/6,
         "texte":"Saturne",
         "positionTexteX":100,
-        "positionTexteY":600,
+        "positionTexteY":300,
         "description":"Saturne est la seule planète qui \npossède une couronne d'anneaux \net elle possède plus d'une \ncentaine de satellite !",
         "path":"assets/saturn.png"
     },
@@ -111,8 +111,8 @@ let data =[
         "tailleReelle":1000000.0,
         "angle":Math.PI/7,
         "texte":"Mars",
-        "positionTexteX":500,
-        "positionTexteY":600, 
+        "positionTexteX":-400,
+        "positionTexteY":0, 
         "description":"La planete rouge !",
         "path":"assets/mars.png"
     }
