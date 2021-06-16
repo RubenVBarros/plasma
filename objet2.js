3611169ecab353573d5a17028dab09c0924fd0df
RubenVBarros <rubenvenancio67@gmail.com>
2021-06-16 12:52:30 +0200

    main: ajout equivalence titre, video dans chaque objet et valeur initiale du slider

--- objet2.js
+++ objet2.js
@@ -2,128 +2,141 @@ let data =[
     {
         "tailleReelle":1e-9,
         "angle":Math.PI/8,
-        "texte":"Molécule de cristal",
+        "texte":"Molécule de cristal (1nm)",
         "positionTexteX":300,
         "positionTexteY":-400,
         "description":"C'est une molécule de cristal, \nune molécule c'est très petit et \nc'est l'assemblage de plusieurs \natomes.",
-        "path":"assets/cristal.png"
+        "path":"assets/cristal.png",
+        "video":""
     },
     {
         "tailleReelle":1e-6,
         "angle":Math.PI/8,
-        "texte":"Globule rouge",
-        "positionTexteX":-1000,
+        "texte":"Globule rouge (1µm)",
+        "positionTexteX":-1200,
         "positionTexteY":0,
         "description":"Les globules rouges permettent \nde transporter l'oxygène vers les \ntissus et elles permettent de \ncapter le gaz carbonique au \nniveau des tissus pour les \néliminer par les voies respiratoires.",
-        "path":"assets/globule-rouge.png"
+        "path":"assets/globule-rouge.png",
+        "video":""
     },
     {
         "tailleReelle":3e-3,
         "angle":Math.PI/7,
-        "texte":"Larve de poisson",
-        "positionTexteX":-1300,
+        "texte":"Larve de poisson (3mm)",
+        "positionTexteX":-1500,
         "positionTexteY":0,
         "description":"La larve de poisson est le premier \nstade de développement pour un \npoisson, après l'éclosion des \noeufs, c'est comme cela que le \npoisson débute sa vie.",
-        "path":"assets/larve.png"
+        "path":"assets/larve.png",
+        "video":""
     },
     {
         "tailleReelle":1e-3,
         "angle":Math.PI/7,
-        "texte":"Poisson zèbre",
+        "texte":"Poisson zèbre (1mm)",
         "positionTexteX":100,
         "positionTexteY":-500,
         "description":"Le poisson zèbre est une espèce \nde poisson très utilisée dans \nle monde scientifique car il aurait \nla particularité de régénérer \ndes parties de son corps en cas \nde blessure, si on pouvait \nappliquer cela aux humains ce \nserait fou !",
-        "path":"assets/poisson.png"
+        "path":"assets/poisson.png",
+        "video":""
     },
     {
         "tailleReelle":2e-1,
         "angle":Math.PI/8,
-        "texte":"Main",
+        "texte":"Main (20 cm)",
         "positionTexteX":-500,
         "positionTexteY":500,
         "description":"Une main, nous en avons deux \nmais une seule fait près de 20 \ncentimètres, rendez-vous compte \nla taille d'un double décimètre \nmais on peut faire tellement de \nchoses avec, que feriez vous si \nvous n'aviez pas de main ?",
-        "path":"assets/main.png"
+        "path":"assets/main.png",
+        "video":""
     },
     {
         "tailleReelle":12,
         "angle":Math.PI/4,
-        "texte":"Bus",
+        "texte":"Bus (12 m)",
         "positionTexteX":1200,
         "positionTexteY":-700,
         "description":"Ca c'est le ScientiBus, c'est la ou \nont lieu les meilleures expériences \nscientifiques de France.",
-        "path":"assets/bus.png"
+        "path":"assets/bus.png",
+        "video":""
     },
     {
         "tailleReelle":128,
         "angle":Math.PI/7,
-        "texte":"Cathédrale de Notre Dame",
+        "texte":"Cathédrale de Notre Dame (128 m)",
         "positionTexteX":-400,
         "positionTexteY":-600,
         "description":"La cathédrale Notre-Dame de \nParis est un monument \nemblématique de Paris bien \nqu'elle ait subit un incendie, son \nhistoire ne s'en trouve pas moins \neffacé. Elle a accueilli le Sacre de \nNapoléon en 1804.",
-        "path":"assets/notre-dame.png"
+        "path":"assets/notre-dame.png",
+        "video":""
     },
     {
-        "tailleReelle":8.8e3,
+        "tailleReelle":18e2,
         "angle":Math.PI/7,
-        "texte":"Ville de Limoges",
+        "texte":"Ville de Limoges (18 km)",
         "positionTexteX":0,
         "positionTexteY":-500,  
         "description":"La ville de Limoges avec ses \nagglomérations autour, c'est la \nville ou vous habitez sûrement !",
-        "path":"assets/limoges.png"
+        "path":"assets/limoges.png",
+        "video":""
     },
     {
-        "tailleReelle":2.592402e6,
+        "tailleReelle":1000000,
         "angle":Math.PI/7,
-        "texte":"France",
-        "positionTexteX":-300,
+        "texte":"France (1000 km)",
+        "positionTexteX":-800,
         "positionTexteY":300,
-        "description":"C'est la France, pays dans lequel \nnous vivons, c'nord le pays de la \nbaguette et du bon vin.",
-        "path":"assets/france.png"
+        "description":"C'est la France, pays dans lequel \nnous vivons, c'est le pays de la \nbaguette et du bon vin.",
+        "path":"assets/france.png",
+        "video":"assets/miss.mp4"
     },
     {
         "tailleReelle":12742000.0,
         "angle":Math.PI/6,
-        "texte":"Terre",
+        "texte":"Terre (12 742 km)",
         "positionTexteX":-400,
-        "positionTexteY":0,  
-        "description":"La Terre, c'est la planète ou nous \nvivons, le saviez-vous que la Terre \eest composé à 70% d'eau, elle \nréunit aussi tout les critères \nnécessaires pour le \ndéveloppement de la vie.",
-        "path":"assets/earth.png"
+        "positionTexteY":-200,  
+        "description":"La Terre, c'est la planète ou nous \nvivons, elle réunit tout les critères\nnécessaires pour le \ndéveloppement de la vie elle est \ncomposé à plus de 70% d'eau !",
+        "path":"assets/earth.png",
+        "video":""
     },
     {
         "tailleReelle":116460000.0,
         "angle":Math.PI/6,
-        "texte":"Saturne",
+        "texte":"Saturne (116 460 km)",
         "positionTexteX":100,
         "positionTexteY":300,
         "description":"Saturne est la seule planète qui \npossède une couronne d'anneaux \net elle possède plus d'une \ncentaine de satellite !",
-        "path":"assets/saturn.png"
+        "path":"assets/saturn.png",
+        "video":""
     },
     {
         "tailleReelle":1.4e9,
         "angle":Math.PI/7,
-        "texte":"Soleil",
+        "texte":"Soleil (1 400 000 km)",
         "positionTexteX":100,
         "positionTexteY":600,  
         "description":"Le Soleil est notre source de \nlumières et de chaleur, le soleil est \ntellement massif qu'il représente \nplus de 99,854% de la masse \ntotale du Système solaire et il \német 20 000 fois plus d'énergie \nque nous en consommons, c'est \ntotalement dingue !",
-        "path":"assets/sun.png"
+        "path":"assets/sun.png",
+        "video":""
     },
     {
-        "tailleReelle":1000000.0,
+        "tailleReelle":2000000.0,
         "angle":Math.PI/7,
-        "texte":"Mars",
-        "positionTexteX":-400,
+        "texte":"Mars (2000 km)",
+        "positionTexteX":-800,
         "positionTexteY":0, 
         "description":"La planete rouge !",
-        "path":"assets/mars.png"
+        "path":"assets/mars.png",
+        "video":""
     },
-    /* test du gif marche pas
     {
         "tailleReelle":1,
-        "angle":Math.PI/7,
-        "texte":"Roi",
-        "positionTexteX":-400,
-        "positionTexteY":0, 
-        "description":"Sa majesté le roi de france !",
-        "path":"assets/roi.gif"
-    }*/
+        "angle":0,
+        "texte":"1 mètre",
+        "positionTexteX":200,
+        "positionTexteY":0,
+        "description":"C'est un mètre",
+        "path":"assets/fleche.png",
+        "video":""
+    },
 ];
--- settings.js
+++ settings.js
@@ -1,9 +1,11 @@
- // ! Ce fichier contient des variables qui sont utilisées dans le code principal et sont modifiables,
- // ! Pour éviter les problèmes si suppression dans le code général ces variables sont mises dans ce fichier. 
- //Variable générale qui donne l'effet de zoom
- let tailleRef = 3779;
- let rayon = 1500;
- let centreX = 700;
+// ! Ce fichier contient des variables qui sont utilisées dans le code principal et sont modifiables,
+// ! Pour éviter les problèmes si suppression dans le code général ces variables sont mises dans ce fichier. 
+let tailleRef = 3779; //Variable générale qui donne l'effet de zoom
+let rayon = 1500;//Vitesse de déplacement des objets
+let centreX = 700;//Position horizontale du point de fuite
 
- //Image de l'arrière plan de l'application (pas fonctionnel encore)
- let bgImage = "assets/rome.jpg";
+//Image de l'arrière plan de l'application
+let bgImage = "assets/montagne.jpg";
+
+//Valeur initiale du curseur correspond a la puissance EXEMPLE : Si on met 10 on va arriver a puissance 10 donc 10 gigamètres etc...
+let valeurInitiale = 1;
